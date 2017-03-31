3e2c21bfabb06f54f0d7de09926a8c007280e7bc
Mark Said Camilleri <markscamilleri@gmail.com>
2017-03-31 15:26:09 +0200

    Added getName()

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,11 +3,15 @@
  * @version 31/03/17.
  */
 public class Hello {
-    public static final String getHello(){
-        return "Hello World";
+    public static final String getName() {
+        return "Denise and Mark Assignment";
     }
     
     public static void main(String[] args) {
         System.out.println(getHello());
+    }
+    
+    public static final String getHello() {
+        return "Hello World";
     }
 }
