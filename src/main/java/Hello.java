8f81cc057a01109949d122e926fdbb713c343b38
Mark Said Camilleri <markscamilleri@gmail.com>
2017-03-31 15:15:51 +0200

    Added first shell code

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * @author mark
+ * @version 31/03/17.
+ */
+public class Hello {
+    public static final String getHello(){
+        return "Hello World";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getHello());
+    }
+}
