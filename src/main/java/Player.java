4b042ed62e3a50e002577a0a4d4e912d8ff8d345
Denise Buttigieg <denise.buttigieg.14@um.edu.mt>
2017-04-13 13:14:34 +0200

    tests for player moves U, D, R, L

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 
+import static java.lang.Character.toLowerCase;
+
 /**
  * @author denise
  * @version 12/04/2017
@@ -8,30 +10,27 @@ import java.util.Random;
 public class Player extends Position{
 
     Position p;
+
     public void init(int mapSize){
         Random random = new Random();
-        p.x = random.nextInt(mapSize);
-        p.y = random.nextInt(mapSize);
+        p = new Position(random.nextInt(mapSize), random.nextInt(mapSize));
     }
 
     public void move(char direction){
+        toLowerCase(direction);
         //I am assuming that the bottom left corner is coordinate 0,0
         //still need to check for mapsize
         switch(direction){
             case 'u':
-            case 'U':
                 p.y++;
                 break;
             case 'd':
-            case 'D':
                 p.y--;
                 break;
             case 'l':
-            case 'L':
                 p.x--;
                 break;
             case 'r':
-            case 'R':
                 p.x++;
                 break;
             default:
--- src/test/java/PlayerMoveTest.java
+++ src/test/java/PlayerMoveTest.java
@@ -2,23 +2,76 @@
  * @author denise
  * @version 12/04/2017.
  */
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerMoveTest extends Player{
 
+    /**
+     *Testing move: up
+     */
+    @Before
+    public void initialisePlayerPos() throws Exception{
+        init(5);
+        Assert.assertNotNull(p.x);
+        Assert.assertNotNull(p.y);
+    }
+
     @Test
-    public void testPlayerMove() throws Exception{
-        Assert.assertEquals('u', p.x = 0, p.y = 1);
-        Assert.assertEquals('U', p.x = 0, p.y = 1);
+    public void testPlayerMove_u() throws Exception{
+        //up entering 'u';
+        move('u');
+        Assert.assertEquals(p.x, p.x++);
+    }
 
-        Assert.assertEquals('d', p.x = 0, p.y = 1);
-        Assert.assertEquals('D', p.x = 0, p.y = 1);
+    @Test
+    public void testPlayerMove_U() throws Exception{
+        //up entering 'U'
+        move('U');
+        Assert.assertEquals(p.y, p.y++);
+    }
 
-        Assert.assertEquals('l', p.x = 0, p.y = 1);
-        Assert.assertEquals('L', p.x = 0, p.y = 1);
+    @Test
+    public void testPlayerMove_d() throws Exception{
+        //down entering 'd'
+        move('d');
+        Assert.assertEquals(p.y, p.y--);
+    }
 
-        Assert.assertEquals('r', p.x = 0, p.y = 1);
-        Assert.assertEquals('R', p.x = 0, p.y = 1);
+    @Test
+    public void testPlayerMove_D() throws Exception{
+        //down entering 'D'
+        move('D');
+        Assert.assertEquals(p.y, p.y--);
+    }
+
+    @Test
+    public void testPlayerMove_l() throws Exception{
+        //left entering 'l'
+        move('l');
+        Assert.assertEquals(p.x, p.x--);
+    }
+
+    @Test
+    public void testPlayerMove_L() throws Exception{
+        //left entering 'L'
+        move('L');
+        Assert.assertEquals(p.x, p.x--);
+    }
+
+    @Test
+    public void testPlayerMove_r() throws Exception{
+        //right entering 'r'
+        move('r');
+        Assert.assertEquals(p.x, p.x++);
+    }
+
+    @Test
+    public void testPlayerMove_R() throws Exception{
+        //right entering 'R'
+        move('R');
+        Assert.assertEquals(p.x, p.x++);
     }
 }
