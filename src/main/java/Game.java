55c683cb9cb776546f0418217becd3c296d2788a
Denise Buttigieg <denise.buttigieg.14@um.edu.mt>
2017-04-15 15:13:44 +0200

    fixing some merge issues

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,34 +1,109 @@
+import java.io.*;
 import java.util.Scanner;
 
 /**
- * Created by denise on 12/04/2017.
+ * @author denise
+ * @version 13/04/2017.
  */
+
+public class Game extends Player{
+    public int numOfPlayers;
+
 public class Game {
     private int numOfPlayers;
     public int turns;
+    public int mapSize;
     public Player players[];
 
-    public void startGame() {
+    /**
+     * Checks that the number of players is valid
+     * @param n the number of players
+     * @return true if the number of players is between 2 and 8, false otherwise
+     */
+
+    public boolean setNumPlayers(int n){
+        return !(n < 2 || n > 8);
+    }
+
+    /**
+     * Generates Game HTML Files for each player
+     */
+    public void generateHTMLFiles(){
+        int playerIndex = 1;
+
+        /**
+         * Deleting all previously created game files
+         */
+
+        File dir = new File("src/gamefiles");
+        for (File file: dir.listFiles()) {
+            if (!file.isDirectory()) {
+                file.delete();
+            }
+        }
+
+        /**
+         * Creating new game files according to the new game
+         */
+
+        for (int i=0; i<numOfPlayers; i++, playerIndex++) {
+            String filename = "map_player_" + playerIndex + ".html";
+            String pathToFile = "src/gamefiles/"+filename;
+
+            FileWriter fileWriter = null;
+            BufferedWriter bufferedWriter = null;
+
+            try {
+                String content = "<html>Hello. \nThis is the html file for player " + playerIndex + "\nTesting if Java is cleaning files AGAIN</html>";
+
+                fileWriter = new FileWriter(pathToFile, true);
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(content);
+
+            } catch (IOException ex) {
+                ex.getMessage();
+            } finally {
+                if (bufferedWriter != null) {
+                    try {
+                        bufferedWriter.close();
+                    } catch (IOException ex) {
+                        ex.getMessage();
+                    }
+                }
+                if (fileWriter != null) {
+                    try {
+                        fileWriter.close();
+                    } catch (IOException ex) {
+                        ex.getMessage();
+                    }
+                }
+            }
+        }
+    }
+
+    public void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        Map map;
+
+        //Ask for Number of players
         System.out.println("Enter the number of players:");
         numOfPlayers = scan.nextInt();
         if (setNumPlayers(numOfPlayers)) {
+            players = new Player[numOfPlayers];
+
             System.out.println("Enter map size: ");
+            mapSize = scan.nextInt();
+            map = new Map();
+            map.setMapSize(mapSize, mapSize);
+
+            for(int i=0; i<numOfPlayers; i++){
+                Player p = new Player();
+                players[i] = p;
+            }
             generateHTMLFiles();
         } else {
             System.out.println("Enter the number of players:");
             numOfPlayers = scan.nextInt();
         }
-    }
-
-    public boolean setNumPlayers(int n){
-        if(n<2 || n>8){
-            return false;
-        }
-        return true;
-    }
-
-    public void generateHTMLFiles(){
-
     }
 }
