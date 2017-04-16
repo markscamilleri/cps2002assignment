2fb2a622bd99a617480edba767b717c8e456ade6
Mark Said Camilleri <markscamilleri@gmail.com>
2017-04-16 20:25:14 +0200

    Added more tests for Position Class, and removed redundant constructor

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -4,11 +4,29 @@
  */
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PositionTest {
+    Position pos;
+    
+    @Before
+    public void setUp() throws Exception{
+        pos = new Position(0,0);
+    }
+    
     @Test
-    public void testPosition() throws Exception{
-        Assert.assertNotNull(Position.class);
+    public void testPositionExists() throws Exception{
+        Assert.assertNotNull(pos);
+    }
+    
+    @Test
+    public void testPositonXCorrect() throws Exception{
+        Assert.assertEquals(0,pos.x);
+    }
+    
+    @Test
+    public void testPositonYCorrect() throws Exception{
+        Assert.assertEquals(0,pos.y);
     }
 }
